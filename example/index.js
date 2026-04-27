ac0da0c13cdf9f692620f2d59b41c1696ae23e3f
tymiskj <tderevenets858@gmail.com>
2026-04-27 14:41:08 +0300

    add usage example for memoization with LRU policy
    (lab3)

--- example/index.js
+++ example/index.js
@@ -1,5 +1,33 @@
-import { randomNumberGenerator, consumeWithTimeout } from 'kpi-async-iterator-lib';
+import { randomNumberGenerator, consumeWithTimeout, memoize } from 'kpi-async-iterator-lib';
 
-console.log("=== Запуск моєї лаби через бібліотеку ===");
+console.log("=== Демонстрація Task 1: Async Iterator ===\n");
 const myRandomIterator = randomNumberGenerator();
-consumeWithTimeout(myRandomIterator, 2);
+await consumeWithTimeout(myRandomIterator, 1);
+
+console.log("\n=== Демонстрація Task 3: Memoization ===");
+
+const slowMathFunction = (n) => {
+  let result = 0;
+  for (let i = 0; i <= n * 10000000; i++) {
+    result += i;
+  }
+  return result;
+};
+
+const memoizedMath = memoize(slowMathFunction, { maxSize: 2, policy: 'lru' });
+
+console.time("Перший виклик (Обчислення)");
+console.log("Результат (10):", memoizedMath(10));
+console.timeEnd("Перший виклик (Обчислення)");
+
+console.time("Другий виклик (З кешу)");
+console.log("Результат (10):", memoizedMath(10));
+console.timeEnd("Другий виклик (З кешу)");
+
+console.time("Третій виклик (Обчислення)");
+console.log("Результат (20):", memoizedMath(20));
+console.timeEnd("Третій виклик (Обчислення)");
+
+console.time("Четвертий виклик (Обчислення, витіснить перше значення через LRU)");
+console.log("Результат (30):", memoizedMath(30));
+console.timeEnd("Четвертий виклик (Обчислення, витіснить перше значення через LRU)");
